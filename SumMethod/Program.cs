8d95b9f844c5fc21d4129bd77eba665570962278
Jessie Monson <jessiemonson1980@gmail.com>
2019-03-14 22:22:10 -0500

    final code

--- SumMethod/Program.cs
+++ SumMethod/Program.cs
@@ -6,10 +6,21 @@ namespace SumMethod
     {
         public static void Main()
         {
+            Console.WriteLine("Pick a number: ");
+            int number = int.Parse(Console.ReadLine());
+            int sum = Sum(number);
+            Console.WriteLine(sum);
+
             Console.ReadLine();
         }
+        public static int Sum(int number)
+        {
+            int sum = (number * (number + 1) / 2);
+            return sum;
 
-        // TODO: Create a method that returns the sum of all integer numbers up to and including given value. 
-        // Use the equation (n * (n + 1)) / 2.
+
+            // TODO: Create a method that returns the sum of all integer numbers up to and including given value. 
+            // Use the equation (n * (n + 1)) / 2.
+        }
     }
 }
